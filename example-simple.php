a33fecd617f3220a6056fc6ef070bca668c6e3b0
Aaron D. Caryl <aarondcaryl@gmail.com>
2011-05-16 00:11:06 -0400

    Updated formatting and readme.

--- example-simple.php
+++ example-simple.php
@@ -1,21 +1,21 @@
 <?php
 
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', true);
-
-require_once('webwatch.class.php');
-
-$watcher = new webwatch();
-
-$watcher->setNotifyEmail('aaronc@noein.com');
-$watcher->setNotifyFromPhone('716-508-0112');
-$watcher->setNotifyToPhone('585-245-4640');
-$watcher->setNotifyType('call');
-$watcher->setTwilioAccountSid('ACd335ad70a6f756a2dd93972b6cb00893');
-$watcher->setTwilioAuthToken('86d2aa6b10341ebb49fcd7bac983909a');
-
-$watcher->watch('http://www.google.com/');
-echo '<br/>';
-$watcher->watch('http://www.google.com/404');
+	ini_set('error_reporting', E_ALL);
+	ini_set('display_errors', true);
+	
+	require_once('webwatch.class.php');
+	
+	$watcher = new webwatch();
+	
+	$watcher->setNotifyEmail('aaronc@noein.com');
+	$watcher->setNotifyFromPhone('716-508-0112');
+	$watcher->setNotifyToPhone('585-245-4640');
+	$watcher->setNotifyType('call');
+	$watcher->setTwilioAccountSid('ACd335ad70a6f756a2dd93972b6cb00893');
+	$watcher->setTwilioAuthToken('86d2aa6b10341ebb49fcd7bac983909a');
+	
+	$watcher->watch('http://www.google.com/');
+	echo '<br/>';
+	$watcher->watch('http://www.google.com/404');
     
 ?>
